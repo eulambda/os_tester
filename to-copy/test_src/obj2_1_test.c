89a70c7568816b71680131795716d05c71d51cfd
name <email@email.email>
2024-06-13 05:28:57 +0900

    fix tests

--- to-copy/test_src/obj2_1_test.c
+++ to-copy/test_src/obj2_1_test.c
@@ -7,31 +7,46 @@
 #include "memlayout.h"
 #include "fcntl.h"
 
+#pragma GCC diagnostic ignored "-Wunused-variable"
+int allocate_only()
+{
+	volatile char stack[1024 * 8];
+	return frees();
+}
+#pragma GCC diagnostic pop
+#pragma GCC diagnostic ignored "-Wunused-but-set-variable"
+int allocate_and_assign()
+{
+	volatile char stack[1024 * 8];
+	for (int i = 0; i < 1024 * 8; i += 4096)
+	{
+		stack[i] = 'A';
+	}
+	return frees();
+}
+#pragma GCC diagnostic pop
 int main(void)
 {
 	// #6 - stack demand paging
 	int init_frees, after_frees, touch_frees;
 
-	init_frees = frees();	
+	init_frees = frees();
 	printf(1, "=== Check initial frees : %d ===\n", init_frees);
 
-	volatile char stack[1024 * 8];
-	
-	after_frees = frees();	
+	after_frees = allocate_only();
 	printf(1, "=== Check after allocating frees : %d ===\n", after_frees);
 
-	for (int i = 0; i < 1024 * 8; i += 4096) {
-        	stack[i] = 'A';
-	}
-
-	touch_frees = frees();
+	touch_frees = allocate_and_assign();
 	printf(1, "=== Check touch frees : %d ===\n", touch_frees);
 
-	if (init_frees - after_frees == 0 && init_frees - touch_frees != 0) {
-        	printf(1, "==== Stack demand paging success ====\n");
-    	} else {
-        	printf(1, "**** Stack demand paging failure ****\n");
-    	}	
-	
+	if (init_frees - after_frees == 0 && init_frees - touch_frees != 0)
+	{
+		printf(1, "==== Stack demand paging success ====\n");
+	}
+	else
+	{
+		printf(1, "**** Stack demand paging failure ****\n");
+	}
+
 	exit();
 }
